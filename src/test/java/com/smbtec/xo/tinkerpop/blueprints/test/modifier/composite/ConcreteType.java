f44c145c6385d31c525aee814ee42d85d98690b1
Lars Martin <lars.martin@smb-tec.com>
2014-10-28 20:59:51 +0100

    add modifier test

---
+++ src/test/java/com/smbtec/xo/tinkerpop/blueprints/test/modifier/composite/ConcreteType.java
@@ -0,0 +1,7 @@
+package com.smbtec.xo.tinkerpop.blueprints.test.modifier.composite;
+
+import com.smbtec.xo.tinkerpop.blueprints.api.annotation.Vertex;
+
+@Vertex("Concrete")
+public interface ConcreteType {
+}
